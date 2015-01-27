369b47e9c173ebb6978ef245c30526431c82918b
Alex Machado <alex_mfarias@hotmail.com>
2015-01-27 08:24:07 -0200

    Create work.bat

---
+++ work.bat
@@ -0,0 +1,8 @@
+start c:\sublime_text.lnk
+start c:\photoshop.lnk
+start c:\chrome.lnk
+start c:\Evernote.lnk
+start c:\wamp\www\
+start http://www.runrun.it
+start http://localhost/
+start http://mail.google.com
